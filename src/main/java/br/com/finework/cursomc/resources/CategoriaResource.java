5fb33b569f6dc3f7047a385e75992b398149acd5
Nilton Dias <ntjdias@gmail.com>
2020-02-23 10:12:04 -0300

    Criando operação de instanciacao

--- src/main/java/br/com/finework/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/com/finework/cursomc/resources/CategoriaResource.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.List;
-import java.util.ArrayList;
 
 import br.com.finework.cursomc.domain.Categoria;
 import br.com.finework.cursomc.services.CategoriaService;
