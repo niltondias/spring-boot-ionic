0c77f5a8b88f6113209aafdba1f92f1904418445
Nilton Dias <ntjdias@gmail.com>
2020-02-23 22:33:38 -0300

    Estado e cidade

---
+++ src/main/java/br/com/finework/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,12 @@
+package br.com.finework.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.finework.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+    
+    
+}
