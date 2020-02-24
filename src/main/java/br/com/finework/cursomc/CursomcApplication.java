3caab4e9d1136e865c4eea718f2961a3cc84730a
Nilton Dias <ntjdias@gmail.com>
2020-02-24 20:20:21 -0300

    Cliente, TipoCliente, telefones e enderecos

--- src/main/java/br/com/finework/cursomc/CursomcApplication.java
+++ src/main/java/br/com/finework/cursomc/CursomcApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.finework.cursomc.domain.Categoria;
 import br.com.finework.cursomc.domain.Cidade;
+import br.com.finework.cursomc.domain.Cliente;
+import br.com.finework.cursomc.domain.Endereco;
 import br.com.finework.cursomc.domain.Estado;
 import br.com.finework.cursomc.domain.Produto;
+import br.com.finework.cursomc.domain.enums.TipoCliente;
 import br.com.finework.cursomc.repositories.CategoriaRepository;
 import br.com.finework.cursomc.repositories.CidadeRepository;
+import br.com.finework.cursomc.repositories.ClienteRepository;
+import br.com.finework.cursomc.repositories.EnderecoRepository;
 import br.com.finework.cursomc.repositories.EstadoRepository;
 import br.com.finework.cursomc.repositories.ProdutoRepository;
 
@@ -30,6 +35,12 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	private EstadoRepository estadoRepository;
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -61,10 +72,20 @@ public class CursomcApplication implements CommandLineRunner {
 		est1.getCidades().addAll(Arrays.asList(c1));
 		est2.getCidades().addAll(Arrays.asList(c2,c3));
 
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		cli1.getTelefones().addAll(Arrays.asList("27363323","93838393"));
+
+		Endereco e1 = new Endereco(null, "Rua das Flores", "300", "Apto 203", "Jardim", "38220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Av Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+
+		cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
+
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 		estadoRepository.saveAll(Arrays.asList(est1,est2));
 		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1,e2));
 
 	}
 
---
+++ src/main/java/br/com/finework/cursomc/repositories/ClienteRepository.java
@@ -0,0 +1,12 @@
+package br.com.finework.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.finework.cursomc.domain.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+    
+    
+}
