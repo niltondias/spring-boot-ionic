37fa061802c7cb65c3a3b9fe8a1970c3128c06d2
Nilton Dias <ntjdias@gmail.com>
2020-02-25 02:10:32 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/br/com/finework/cursomc/CursomcApplication.java
+++ src/main/java/br/com/finework/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package br.com.finework.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import br.com.finework.cursomc.domain.Cidade;
 import br.com.finework.cursomc.domain.Cliente;
 import br.com.finework.cursomc.domain.Endereco;
 import br.com.finework.cursomc.domain.Estado;
+import br.com.finework.cursomc.domain.Pagamento;
+import br.com.finework.cursomc.domain.PagamentoComBoleto;
+import br.com.finework.cursomc.domain.PagamentoComCartao;
+import br.com.finework.cursomc.domain.Pedido;
 import br.com.finework.cursomc.domain.Produto;
+import br.com.finework.cursomc.domain.enums.EstadoPagamento;
 import br.com.finework.cursomc.domain.enums.TipoCliente;
 import br.com.finework.cursomc.repositories.CategoriaRepository;
 import br.com.finework.cursomc.repositories.CidadeRepository;
 import br.com.finework.cursomc.repositories.ClienteRepository;
 import br.com.finework.cursomc.repositories.EnderecoRepository;
 import br.com.finework.cursomc.repositories.EstadoRepository;
+import br.com.finework.cursomc.repositories.PagamentoRepository;
+import br.com.finework.cursomc.repositories.PedidoRepository;
 import br.com.finework.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -41,6 +49,12 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+
+	@Autowired
+	PedidoRepository pedidoRepository;
+
+	@Autowired
+	PagamentoRepository pagamentoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -80,6 +94,19 @@ public class CursomcApplication implements CommandLineRunner {
 
 		cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
 
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/M/yyyy HH:mm");
+
+		Pedido ped1 = new Pedido(null, sdf.parse("30/02/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+
+		Pagamento pgto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pgto1);
+
+		Pagamento pgto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pgto2);
+
+		cli1.getPedidos().addAll(Arrays.asList(ped1,ped2));
+/*** 
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 		estadoRepository.saveAll(Arrays.asList(est1,est2));
@@ -87,6 +114,9 @@ public class CursomcApplication implements CommandLineRunner {
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1,e2));
 
+		pedidoRepository.saveAll(Arrays.asList(ped1,ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pgto1,pgto2));
+***/
 	}
 
 }
