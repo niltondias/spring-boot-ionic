15379eb558984e39ca373753cd8f86b798df703e
Nilton Dias <ntjdias@gmail.com>
2020-02-28 16:36:37 -0300

    Listando todas categorias com DTO

--- src/main/java/br/com/finework/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/com/finework/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package br.com.finework.cursomc.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.finework.cursomc.domain.Categoria;
+import br.com.finework.cursomc.dto.CategoriaDTO;
 import br.com.finework.cursomc.services.CategoriaService;
 
 @RestController
@@ -47,5 +50,13 @@ public class CategoriaResource {
     public ResponseEntity<Void> delete( @PathVariable Integer id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping( method = RequestMethod.GET)
+    public ResponseEntity<List<CategoriaDTO>> findAll() {
+
+        List<Categoria> list = service.findAll();
+        List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList()); //Varrendo todo o ojeto list convertendo de Categoria para CategoriaDTO
+        return ResponseEntity.ok().body(listDto);
     }
 }
--- src/main/java/br/com/finework/cursomc/services/CategoriaService.java
+++ src/main/java/br/com/finework/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package br.com.finework.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -44,6 +45,10 @@ public class CategoriaService {
         } catch ( DataIntegrityViolationException e ) {
             throw new DataIntegrityException("Não é possível excluir uma categoria que possue produtos");
         }
+    }
+
+    public List<Categoria> findAll() {
+        return repo.findAll();
     }
 
 }
