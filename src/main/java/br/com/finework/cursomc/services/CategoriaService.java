4856c966be1db6543523a620e82f91ed9f1795ce
Nilton Dias <ntjdias@gmail.com>
2020-02-28 12:13:22 -0300

    Atualizando Categoria com PUT, outros pequenos ajustes

--- src/main/java/br/com/finework/cursomc/services/CategoriaService.java
+++ src/main/java/br/com/finework/cursomc/services/CategoriaService.java
@@ -14,7 +14,8 @@ public class CategoriaService {
     
     @Autowired
     private CategoriaRepository repo;
-    public Categoria buscar(Integer id) {
+
+    public Categoria find(Integer id) {
         Optional<Categoria> obj = repo.findById(id);
 
         return obj.orElseThrow(() -> new ObjectNotFoundException(
@@ -27,6 +28,11 @@ public class CategoriaService {
     public Categoria insert( Categoria obj ) {
         obj.setId(null);  // colocando null o método save considera um novo objeto no banco.
         return repo.save(obj);
-
     }
+
+    public Categoria update( Categoria obj ) {
+        find(obj.getId());  
+        return repo.save(obj);
+    }
+
 }
