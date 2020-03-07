d5c5540fa849033d5acb52050c26546f7fef1b1f
Nilton Dias <ntjdias@gmail.com>
2020-03-06 21:56:54 -0300

    Validacao customizada: email nao repetido na atualizacao de Cliente

--- src/main/java/br/com/finework/cursomc/dto/ClienteDTO.java
+++ src/main/java/br/com/finework/cursomc/dto/ClienteDTO.java
@@ -8,54 +8,52 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import br.com.finework.cursomc.domain.Cliente;
+import br.com.finework.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	
+	@NotEmpty(message="Preenchimento obrigat?rio")
+	@Length(min=5, max=120, message="O tamanho deve ser entre 5 e 120 caracteres")
+	private String nome;
+	
+	@NotEmpty(message="Preenchimento obrigat?rio")
+	@Email(message="Email inv?lido")
+	private String email;
+	
+	public ClienteDTO() {
+	}
 
-    private Integer id;
+	public ClienteDTO(Cliente obj) {
+		id = obj.getId();
+		nome = obj.getNome();
+		email = obj.getEmail();
+	}
+	
+	public Integer getId() {
+		return id;
+	}
 
-    @NotEmpty( message = "Preenchimento obrigat?rio" )
-    @Length( min=5, max = 120, message = "O tamanho deve ser entre 5 e 120" )
-    private String nome;
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-    @NotEmpty( message = "Preenchimento obrigat?rio" )
-    @Email( message = "Email inválido" )
-    private String email;
+	public String getNome() {
+		return nome;
+	}
 
-    public ClienteDTO() {
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    public ClienteDTO( Cliente obj ) {
-        id = obj.getId();
-        nome = obj.getNome();
-        email = obj.getEmail();
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    
- 
+	public void setEmail(String email) {
+		this.email = email;
+	}
 }
