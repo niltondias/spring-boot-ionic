3cb3e32d8f5442e0cd4a0116aed2d5a3b59e01cc
Nilton Dias <ntjdias@gmail.com>
2020-03-13 12:37:10 -0300

    Subtotal e total do Pedido

--- src/main/java/br/com/finework/cursomc/domain/Pedido.java
+++ src/main/java/br/com/finework/cursomc/domain/Pedido.java
@@ -102,6 +102,15 @@ public class Pedido implements Serializable {
         this.itens = itens;
     }
 
+    public double getValorTotal() {
+        double soma = 0.00;
+        for( ItemPedido ip : itens ) {
+            soma = soma + ip.getSubTotal();
+        }
+
+        return soma;
+    }
+
     @Override
     public int hashCode() {
         final int prime = 31;
