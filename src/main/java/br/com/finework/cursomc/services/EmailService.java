e43bd4cdee1fa08e01d1ae306c27c9f52ce2ac32
Nilton Dias <ntjdias@gmail.com>
2020-03-16 15:05:02 -0300

    Email Html

--- src/main/java/br/com/finework/cursomc/services/EmailService.java
+++ src/main/java/br/com/finework/cursomc/services/EmailService.java
@@ -1,5 +1,7 @@
 package br.com.finework.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import br.com.finework.cursomc.domain.Pedido;
@@ -7,7 +9,9 @@ import br.com.finework.cursomc.domain.Pedido;
 public interface EmailService {
 
     void sendOrderConfirmationEmail(Pedido obj );
-
     void sendEmail( SimpleMailMessage msg );
+
+    void sendOrderConfirmationHtmlEmail(Pedido obj);
+    void sendHtmlEmail(MimeMessage msg);
 
 }
