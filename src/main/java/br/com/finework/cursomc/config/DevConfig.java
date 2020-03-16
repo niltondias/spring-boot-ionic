cf3300cc9e117108de2c43c26864b52bc6ce10e2
Nilton Dias <ntjdias@gmail.com>
2020-03-16 11:27:03 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/br/com/finework/cursomc/config/DevConfig.java
+++ src/main/java/br/com/finework/cursomc/config/DevConfig.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import br.com.finework.cursomc.services.DBService;
+import br.com.finework.cursomc.services.EmailService;
+import br.com.finework.cursomc.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -28,5 +30,10 @@ public class DevConfig {
 
 		dbService.instantiateTestDatabase();
 		return true;
+	}
+
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 }
