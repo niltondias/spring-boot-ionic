99b3bbaa02b8f7869b8e5ea26f37cd9b35190ce0
Nilton Dias <ntjdias@gmail.com>
2020-03-18 11:40:55 -0300

    Configuração inicial do Spring Security

---
+++ src/main/java/br/com/finework/cursomc/config/SecurityConfig.java
@@ -0,0 +1,58 @@
+package br.com.finework.cursomc.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    
+    private static final String[] PUPLIC_MATCHERS = {
+        "/h2-console/**"
+    };
+    
+    private static final String[] PUPLIC_MATCHERS_GET = {
+        "/produtos/**",
+        "/categorias/**"
+    };
+
+    @Override
+    protected void configure(HttpSecurity http ) throws Exception {
+
+        if(Arrays.asList(env.getActiveProfiles()).contains("test")) {
+            http.headers().frameOptions().disable();
+        }
+
+        http.cors().and().csrf().disable();
+        http.authorizeRequests()
+            .antMatchers(HttpMethod.GET, PUPLIC_MATCHERS_GET).permitAll()
+            .antMatchers(PUPLIC_MATCHERS).permitAll()
+            .anyRequest().authenticated();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Autowired
+    Environment env;
+
+    @Bean
+    CorsConfigurationSource configurationSource() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return source;
+    }
+
+
+
+}
